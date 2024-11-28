a12014f502551226c9d0ec47a9eebd9b08da4e2f
HR <142994109+Omitofo@users.noreply.github.com>
2024-11-28 22:52:43 +0100

    squizubleeee

--- src/components/HeroD.js
+++ src/components/HeroD.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import backgroundpic from '../assets/two.png';
-import SunPic from '../assets/sun.png';
 import Space from '../assets/abyss.jpg';
-import Planet1 from '../assets/planet1.png';
 import styles from '../moduleCSS/HeroD.module.css';
 
 const HeroD = ({ changeHero }) => {
@@ -13,25 +11,21 @@ const HeroD = ({ changeHero }) => {
             <button onClick={() => changeHero('A')} className={styles.heroButton}></button>
 
             <div className={styles.slider}>
-                <div className={styles.item} style={{ '--position': 1 }}><img src={SunPic} alt="sun" /></div>
+               
                 <div className={styles.item} style={{ '--position': 2 }}><h2 className={styles.words}>Welcome</h2></div>
-                <div className={styles.item} style={{ '--position': 3 }}><img src={Planet1} alt="planet" /></div>
+               
                 <div className={styles.item} style={{ '--position': 4 }}><h2 className={styles.words}>Bienvenido</h2></div>
-                <div className={styles.item} style={{ '--position': 5 }}><img src={SunPic} alt="sun" /></div>
+                
                 <div className={styles.item} style={{ '--position': 6 }}><h2 className={styles.words}>欢迎 </h2></div>
-                <div className={styles.item} style={{ '--position': 7 }}><img src={Planet1} alt="planet" /></div>
+                
                 <div className={styles.item} style={{ '--position': 8 }}><h2 className={styles.words}>Willkommen</h2></div>
-                <div className={styles.item} style={{ '--position': 9 }}><img src={SunPic} alt="sun" /></div>
+              
                 <div className={styles.item} style={{ '--position': 10 }}><h2 className={styles.words}>ようこそ</h2></div>
-                <div className={styles.item} style={{ '--position': 11 }}><img src={Planet1} alt="planet" /></div>
-                <div className={styles.item} style={{ '--position': 12 }}><h2 className={styles.words}>ようこそ</h2></div>
-                <div className={styles.item} style={{ '--position': 13 }}><img src={Planet1} alt="planet" /></div>
+              
+                <div className={styles.item} style={{ '--position': 12 }}><h2 className={styles.words}>Maligayang pagdating </h2></div>
+            
                 <div className={styles.item} style={{ '--position': 14 }}><h2 className={styles.words}>환영합니다</h2></div>
-                <div className={styles.item} style={{ '--position': 15 }}><img src={Planet1} alt="planet" /></div>
-            </div>
-
-            <div className={styles.declaration}>
-                <h3 className={styles.declarationtext}>I AM HERE TO CONQUER. OBEY, OR PERISH!</h3>
+               {/* <div className={styles.item} style={{ '--position': 15 }}><img src={Planet1} alt="planet" /></div>*/}
             </div>
 
             <div className={styles.content}>
