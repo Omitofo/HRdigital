15390545dab890cfa1f04bad739c0577f7bf3432
HR <142994109+Omitofo@users.noreply.github.com>
2024-12-12 13:47:45 +0100

    update

--- src/components/CookieHeader.js
+++ src/components/CookieHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../moduleCSS/CookieHeader.module.css';
 import HomeIcon from '../assets/home.png';
 import ContactIcon from '../assets/contact.png';
-import AboutIcon from '../assets/aboutUs.png';
+import ShoppingCartIcon from '../assets/shoppingCart.png';
 
 
 const CookieHeader = () => {
@@ -12,15 +12,17 @@ const CookieHeader = () => {
             <div className={styles.desktopHeader}>
                 <h2> HOME </h2>
                 <h2> ABOUT US</h2>
-                <h2>CONTACT</h2>
+                <h2> CONTACT</h2>
                 <h2> ALL COOKIES</h2>
+                <button className={styles.navButton}> <img src={ShoppingCartIcon} alt="Shopping Cart" className={styles.iconImage} /></button>
+
             </div>
 
             {/* Mobile Header (Sticky Bottom) */}
             <div className={styles.mobileHeader}>
                 <button className={styles.navButton}> <img src={HomeIcon} alt="Home" className={styles.iconImage} /></button>
-                <button className={styles.navButton}> <img src={AboutIcon} alt="About Us" className={styles.iconImage} /></button>
-                <button className={styles.navButton}>  <img src={ContactIcon} alt="About Us" className={styles.iconImage} /></button>
+                <button className={styles.navButton}> <img src={ShoppingCartIcon} alt="Shopping Cart" className={styles.iconImage} /></button>
+                <button className={styles.navButton}>  <img src={ContactIcon} alt="Contact" className={styles.iconImage} /></button>
                 <button className={styles.navButton}>🍪</button>
 
             </div>
